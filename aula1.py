d088c2510d4c373abf71f8e2a9ac7056dae60236
RenatoABecker <reaugustobe@gmail.com>
2026-03-05 22:40:40 -0300

    Add files via upload

---
+++ aula1.py
@@ -0,0 +1,26 @@
+print ("hello world")
+print ("olá tudo bem?")
+
+nome = input ("insira seu nome:")
+print ("renato"+ nome)
+
+#print (type (str nome))
+numero = 1.5
+test = 1,5
+print (numero)
+print (test) 
+print (type(numero))
+print (type(test))
+
+x = input ("idade")
+print (x)
+print (type(x))
+
+x = int(x)
+print (x+2)
+
+
+
+
+
+
---
+++ aula2.py
@@ -0,0 +1,10 @@
+vit=input("escreva suas vitórias")
+der=input("escreva suas derrotas")
+
+vit = (int(vit)) 
+der = (int(der)) 
+
+res=input("resultado") 
+res = (int(res))
+
+res = vit
